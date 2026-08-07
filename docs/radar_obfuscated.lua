7b3830a150e6fe742aabdf7318ea291596695263
3958496320 <3958496320@users.noreply.github.com>
2026-08-07 13:36:14 +0000

    feat: 修复混淆报错 OnClientEvent
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ docs/radar_obfuscated.lua
@@ -0,0 +1,256 @@
+--[[============================================================
+  苍米独家混淆 · CangMi Exclusive Obfuscator
+  12-Layer Ultimate Luau Obfuscator
+  严禁二次分发 / 改头换面 / 冒充自有作品
+  Copyright (C) CangMi. All rights reserved.
+  水印指纹: 0xC4CC-M1-EXCLUSIVE
+============================================================]]
+local KORdjjZK7 = {}
+local function fphJWAMMYNoX(C5Ct2LBtKNdN8V, WnOnqQfkp, JekaACSMEs, osTsS2De)
+    local qYYOLQZ6mBh = C5Ct2LBtKNdN8V .. string.char(WnOnqQfkp, JekaACSMEs, osTsS2De)
+    local Pj5r_jedkYt = KORdjjZK7[qYYOLQZ6mBh]
+    if Pj5r_jedkYt then
+        return Pj5r_jedkYt
+    end
+    local ChDStSz7rGIF, fMc3BVuMqbfo8R0 = {}, string.len(C5Ct2LBtKNdN8V)
+    for WR_rKcWW = 1, fMc3BVuMqbfo8R0 do
+        local EHPC5rlLBO = string.byte(C5Ct2LBtKNdN8V, WR_rKcWW)
+        EHPC5rlLBO = (((EHPC5rlLBO ~ osTsS2De) - JekaACSMEs) % 256) ~ WnOnqQfkp
+        ChDStSz7rGIF[WR_rKcWW] = string.char(EHPC5rlLBO)
+    end
+    local fEwAvuQg1kvASFs = table.concat(ChDStSz7rGIF)
+    KORdjjZK7[qYYOLQZ6mBh] = fEwAvuQg1kvASFs
+    return fEwAvuQg1kvASFs
+end
+do
+    _G[fphJWAMMYNoX("\238\029\014\023\2371\031\001\016", 98, 224, 10)] = function (_WJcDfuquh9Xz2, Q2IDAdmH)
+        return _WJcDfuquh9Xz2 .. Q2IDAdmH
+    end
+end
+local NWFOCWdnrJi6s = (_G[fphJWAMMYNoX("\165\150\133\140\166j\148\186\139", 85, 72, 222)](fphJWAMMYNoX("4WQ+]_+Wp)rZ*[[", 18, 248, 198), fphJWAMMYNoX("\173\252\141", 72, 90, 165)))
+local SFhb9OMZpTktJaJ = false
+local fo05hD7hP0jF6T = {}
+do
+    do
+        local GTrA0E9EpDJj, m5MGV3i2erXY1A = pcall((function ()
+            return game
+        end))
+        if GTrA0E9EpDJj and not (type(m5MGV3i2erXY1A) == "userdata") then
+            SFhb9OMZpTktJaJ = true
+        end
+    end
+    do
+        local YGr_asUt0LLQF, CIEwvJWIyD = pcall((function ()
+            return workspace
+        end))
+        if YGr_asUt0LLQF and not (type(CIEwvJWIyD) == "userdata") then
+            SFhb9OMZpTktJaJ = true
+        end
+    end
+    do
+        local m9SpKyo1XAcuV, T5ZyyQY_P9jF8ci = pcall((function ()
+            return print
+        end))
+        if m9SpKyo1XAcuV and not (type(T5ZyyQY_P9jF8ci) == "function") then
+            SFhb9OMZpTktJaJ = true
+        end
+    end
+end
+do
+    fo05hD7hP0jF6T.FzpjPSa4W2X3gX = (fo05hD7hP0jF6T.FzpjPSa4W2X3gX or 0) + 1
+    fo05hD7hP0jF6T.olZ8SHDdJp51hD = (fo05hD7hP0jF6T.olZ8SHDdJp51hD or 0) + 1
+    fo05hD7hP0jF6T.ZDQHJMu7W4 = (fo05hD7hP0jF6T.ZDQHJMu7W4 or 0) + 1
+    fo05hD7hP0jF6T.N_U4GB05JC4kV2E = (fo05hD7hP0jF6T.N_U4GB05JC4kV2E or 0) + 1
+end
+do
+    local g21CgH5DaUjA2 = 944619211
+    g21CgH5DaUjA2 = g21CgH5DaUjA2 ~ 20
+    local eEY9qvNSzP = 328146801
+    eEY9qvNSzP = eEY9qvNSzP ~ 181
+    local v1R1Iy8uO = 836395077
+    v1R1Iy8uO = v1R1Iy8uO ~ 232
+    local IcNepOR5soVfB9g = 837489948
+    IcNepOR5soVfB9g = IcNepOR5soVfB9g ~ 219
+    local CkSt4bcuY = 835064486
+    CkSt4bcuY = CkSt4bcuY ~ 87
+    local swxBjpHA = 58201215
+    swxBjpHA = swxBjpHA ~ 22
+    local klfN2yNRpF = 814167346
+    klfN2yNRpF = klfN2yNRpF ~ 72
+    local oDOqDqQa4Z = 86453688
+    oDOqDqQa4Z = oDOqDqQa4Z ~ 194
+    local sRIkeC7wLtO8BSq = 1016964633
+    sRIkeC7wLtO8BSq = sRIkeC7wLtO8BSq ~ 172
+    local tmy1EgpyKwKsSsb = 245957461
+    tmy1EgpyKwKsSsb = tmy1EgpyKwKsSsb ~ 200
+    local raK2RXV9d5MVF0 = 805965556
+    raK2RXV9d5MVF0 = raK2RXV9d5MVF0 ~ 102
+    local XZoMZwoOmNqR = 259963764
+    XZoMZwoOmNqR = XZoMZwoOmNqR ~ 204
+    local Og4OPctYCc = 732788374
+    Og4OPctYCc = Og4OPctYCc ~ 4
+    local Uif5suVAlmiYI = 579997523
+    Uif5suVAlmiYI = Uif5suVAlmiYI ~ 158
+    local HG5r0kq5_7E8Z = 523692741
+    HG5r0kq5_7E8Z = HG5r0kq5_7E8Z ~ 193
+end
+do
+    local V2vZhD8ejW8o = 0
+    for ZJxfYzaqIhDxRV = 1, 168 do
+        V2vZhD8ejW8o = V2vZhD8ejW8o + 1
+    end
+end
+do
+    local Ly_O7xgRoEbN, NoPeO_D5StPAh = pcall((function ()
+        return debug.getinfo(1)
+    end))
+    if (Ly_O7xgRoEbN and NoPeO_D5StPAh) and NoPeO_D5StPAh.what then
+        fo05hD7hP0jF6T.stack = 1
+    end
+end
+do
+    local c7ctFhgp3I = nil
+    local SxnCC7p1, g9RxI49Pwdzrm = pcall((function ()
+        local yDxQ7VS7IA = fphJWAMMYNoX("\243\128\241\240\243\135\197\255\240\135\130\241\140\134\135\205\253\204\197\243\128\241\240\243\135\197\205\253\197\247\197451\204\197\202\1974\197\128\135\129", 104, 178, 63)
+        local APg0FNA77r = loadstring(yDxQ7VS7IA)
+        if APg0FNA77r then
+            c7ctFhgp3I = APg0FNA77r()
+        end
+    end))
+    if not (SxnCC7p1 and c7ctFhgp3I) then
+        c7ctFhgp3I = function (_DfQnPOM9)
+            return (_DfQnPOM9 ~ 104) + 1
+        end
+    end
+    do
+        local dYvpiYKn = c7ctFhgp3I(10274570)
+    end
+end
+do
+    local WJnLn_2ov = fphJWAMMYNoX("\136\233\227\189\239\241\189\233\196\187\198\238\190\237\237\190\237\158", 170, 149, 95)
+    local Y4Mar19jiI = NWFOCWdnrJi6s
+    local ZlhQ2biawYYp = ((type(Y4Mar19jiI) == "string") and (#Y4Mar19jiI == #WJnLn_2ov)) and (Y4Mar19jiI == WJnLn_2ov)
+    if not ZlhQ2biawYYp then
+        (function ()
+            local blqdiVAHhVeEC = (debug and debug.getinfo) and debug.getinfo(2, "S")
+            local GLgCGo7NcUY52 = (blqdiVAHhVeEC and blqdiVAHhVeEC.source) or ""
+            pcall((function ()
+                if GLgCGo7NcUY52.sub(1, 1) == "@" then
+                    local DP8bdE1zBRgF = GLgCGo7NcUY52.sub(2)
+                    if os and os.remove then
+                        pcall(os.remove, DP8bdE1zBRgF)
+                    end
+                    if delfile then
+                        pcall(delfile, DP8bdE1zBRgF)
+                    end
+                    if writefile then
+                        pcall(writefile, DP8bdE1zBRgF, "")
+                    end
+                end
+            end))
+            pcall((function ()
+                if _G then
+                    for e4fuMjeirNOLJTu in pairs(_G) do
+                        _G[e4fuMjeirNOLJTu] = nil
+                    end
+                end
+            end))
+            while true do
+                error("watermark broken")
+            end
+        end)()
+    end
+end
+local MHsDGMBgYSh1PP = false
+do
+    local BC3oAv_DzAU, uBuQqx8jR = pcall((function ()
+        return os[fphJWAMMYNoX("\184\185\132\184\128", 171, 27, 91)]()
+    end))
+    local ef0ffBgVVxZiJdL = 0
+    for QhAw2Q7WB25Ud = 1, 9 do
+    end
+    local MtwgKGnjQEo1, w1JxhWrKo = pcall((function ()
+        return os[fphJWAMMYNoX("\216\215\212\216\208", 200, 144, 227)]()
+    end))
+    if ((BC3oAv_DzAU and MtwgKGnjQEo1) and uBuQqx8jR) and (w1JxhWrKo and ((w1JxhWrKo - uBuQqx8jR) > 0.4589)) then
+        MHsDGMBgYSh1PP = true
+    end
+end
+do
+    local J9X_NNRPJbM6Q0, blrSWt5vwal2 = pcall((function ()
+        return debug[fphJWAMMYNoX("\010\020\007\008\013\021\002", 224, 105, 250)](2, fphJWAMMYNoX("\164\155", 159, 192, 40))
+    end))
+    if (J9X_NNRPJbM6Q0 and blrSWt5vwal2) and (blrSWt5vwal2[fphJWAMMYNoX("\180IF\181", 237, 239, 61)] == fphJWAMMYNoX("=", 222, 42, 250)) then
+        MHsDGMBgYSh1PP = true
+    end
+    if J9X_NNRPJbM6Q0 and (blrSWt5vwal2 and (string[fphJWAMMYNoX("UZ]W", 205, 208, 46)](blrSWt5vwal2[fphJWAMMYNoX("\252\240\006\253\236\246", 226, 106, 7)], fphJWAMMYNoX("\"", 46, 189, 237)))) then
+        MHsDGMBgYSh1PP = true
+    end
+end
+do
+    local KQzejVO9bf = pcall((function ()
+        return 1
+    end))
+    local yADvkxtUuX7 = pcall((function ()
+        error(fphJWAMMYNoX("==\000\002\237\018\019==", 86, 201, 239))
+    end))
+    if KQzejVO9bf and yADvkxtUuX7 then
+        MHsDGMBgYSh1PP = true
+    end
+end
+local djkWNdRfr = false
+do
+    local QBFMCArnWGhwBhs, HQtcozMdant7nXi = pcall((function ()
+        return debug
+    end))
+    if (QBFMCArnWGhwBhs and (type(HQtcozMdant7nXi) == fphJWAMMYNoX("@uzxq", 106, 206, 172))) and HQtcozMdant7nXi[fphJWAMMYNoX("\166\168\183\164\189\165\190", 222, 189, 208)] then
+        djkWNdRfr = true
+    end
+    local suhaFVBliyIT, GJ0QZwez2Vc = pcall(getfenv)
+    if suhaFVBliyIT and GJ0QZwez2Vc[fphJWAMMYNoX("ECPBCZR", 64, 69, 41)] then
+        djkWNdRfr = true
+    end
+    local bD5e2uKBKzTOAs, zb8ukZ7ND = pcall((function ()
+        return hookfunction
+    end))
+    if bD5e2uKBKzTOAs and (type(zb8ukZ7ND) == fphJWAMMYNoX("|oDAnKED", 202, 180, 28)) then
+        djkWNdRfr = true
+    end
+end
+do
+    local fB1gpBLzHfz2t, ovXkeGOhHGm4X, wU8_P_jpgjqml = 4628628, 0, {}
+    for WWPel7XOFD = 1, 11 do
+        fB1gpBLzHfz2t = (fB1gpBLzHfz2t * 1103515245) ~ 12345
+        ovXkeGOhHGm4X = ovXkeGOhHGm4X + ((fB1gpBLzHfz2t >> 16) % 1000)
+        wU8_P_jpgjqml[fB1gpBLzHfz2t % 32] = ovXkeGOhHGm4X
+    end
+end
+local Gj8dCgZ40v9TfGP = (function ()
+    do
+        do
+            local R63KPONu29OujCe = true
+            R63KPONu29OujCe = R63KPONu29OujCe and not (1 == 2)
+        end
+    end
+    local seP291fNMGyDL, C5ZKPmuM = pcall(function ()
+        do
+            local COtYrLdwGetDCcd = (((fphJWAMMYNoX("\028", 220, 121, 57) .. fphJWAMMYNoX("\199", 52, 210, 236)) .. fphJWAMMYNoX(")", 118, 90, 79)) .. fphJWAMMYNoX("\147", 211, 204, 224)) .. fphJWAMMYNoX("\131", 59, 58, 7)
+            COtYrLdwGetDCcd = COtYrLdwGetDCcd .. fphJWAMMYNoX("", 169, 50, 103)
+        end
+        return getfenv()
+    end)
+    if seP291fNMGyDL and C5ZKPmuM then
+        return C5ZKPmuM
+    end
+    return _G
+end)()
+local cpTCCHHNkx = {}
+cpTCCHHNkx[(_G[fphJWAMMYNoX("\226\147\130\237\227\143\149\159\236", 131, 103, 174)](_G[fphJWAMMYNoX("\248\011\024\241\251\023\009\007\246", 252, 216, 138)](fphJWAMMYNoX("\250", 85, 241, 8), fphJWAMMYNoX("\171\163\163\170", 30, 225, 249)), fphJWAMMYNoX("\193", 67, 46, 149)))] = function (self, syAXvRMdYOPve)
+    self[(_G[fphJWAMMYNoX("\201\218\233\224\202\142\216\254\239", 247, 252, 68)](fphJWAMMYNoX("\232\012", 10, 28, 153), fphJWAMMYNoX("\230\209\230\213", 112, 89, 187)))] = syAXvRMdYOPve[fphJWAMMYNoX("\157\140\135`p", 202, 81, 112)]
+    self[fphJWAMMYNoX("<xy", 156, 252, 131)] = syAXvRMdYOPve[(_G[fphJWAMMYNoX("<I\\C9mK]N", 30, 99, 231)](fphJWAMMYNoX("Jdkke", 148, 49, 66), fphJWAMMYNoX("SUm", 12, 182, 112)))]
+end
+cpTCCHHNkx:Toggle({Title = fphJWAMMYNoX("\131\205\209\130\168\168\129\213\173\129\173\201", 1, 134, 237), Value = false, Callback = function (gJRysqU1Q85M2)
+    getgenv()[(_G[fphJWAMMYNoX("\131\148\163\170\1320\146\160\145", 207, 138, 176)](fphJWAMMYNoX("e|y\001", 185, 241, 190), fphJWAMMYNoX("\168\187\182\187H", 189, 172, 51)))] = gJRysqU1Q85M2
+end})
+local vfLQj5wt8P = {Alpha = CFrame[fphJWAMMYNoX("\141\184\138", 94, 111, 18)](-1197, 65, -4790), Bravo = CFrame[fphJWAMMYNoX("\164\173\195", 243, 171, 236)](-220, (65), -4919), Charlie = (CFrame)[fphJWAMMYNoX("\138\129\147", 85, 160, 81)](797, 65, -4740), Delta = CFrame[fphJWAMMYNoX("\190\137\151", 170, 218, 32)](2044, 65, -3984), Echo = CFrame[fphJWAMMYNoX("\243\142\248", 185, 231, 77)](2742, 65, -3031), Foxtrot = CFrame[fphJWAMMYNoX("\151\156\238", 130, 80, 171)](3045, 65, -1788), Golf = CFrame[fphJWAMMYNoX("3\008\026", 105, 84, 104)](3376, 65, -(562)), Hotel = CFrame[fphJWAMMYNoX("\167\172\158", 179, 76, 142)](3290, 65, 587), Juliet = CFrame[fphJWAMMYNoX("\237\026\228", 33, 50, 108)](2955, 65, 1804), Kilo = CFrame[fphJWAMMYNoX("\212\221\175", 171, 241, 98)](2569, 65, 2926), Lima = CFrame[fphJWAMMYNoX("hcq", 174, 192, 232)](989, 65, 3419), Omega = (CFrame)[fphJWAMMYNoX("stj", 45, 158, 146)](-319, 65, 3932), Romeo = (CFrame)[fphJWAMMYNoX("\005\030,", 78, 104, 141)](-1479, 65, 3722), Sierra = CFrame[fphJWAMMYNoX("\247\250\236", 214, 1, 78)](-(2528), 65, 2549), Tango = CFrame[fphJWAMMYNoX("5\010\028", 74, 54, 111)](-3018, 65, 1503), Victor = CFrame[fphJWAMMYNoX("\165\216\206", 202, 149, 156)](-3587, 65, 634), Yankee = CFrame[fphJWAMMYNoX("aXJ", 168, 83, 120)](-3957, 65, -287), Zulu = CFrame[fphJWAMMYNoX("#$\218", 114, 114, 173)](-4049, 65, -1334)};
+(getgenv())[(_G[fphJWAMMYNoX("\174\159\142\145\175\235\153{\136", 55, 131, 122)](_G[fphJWAMMYNoX("gv\135\140f*t\154\141", 204, 231, 246)](fphJWAMMYNoX("uLPN", 204, 189, 44), fphJWAMMYNoX("\186\159\149\148", 169, 22, 73)), fphJWAMMYNoX("\002", 132, 170, 163)))] = vfLQj5wt8P
